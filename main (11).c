16222ea44c90674da8fb08327b39e36aed817ddc
221710302048 <59171027+221710302048@users.noreply.github.com>
2020-01-03 10:37:51 +0530

    Add files via upload

---
+++ main (11).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,k;
+    for(i=0;i<=5;i++)
+    {
+        for(k=5;k>=i;k--)
+        printf(" ");
+        for(j=i;j>=0;j--)
+        {
+            printf("%d",j);
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+        }
+        }
